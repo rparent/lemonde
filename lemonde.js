166c0a18d3b9867f633ebad36838862f6375005a
Juliette Spiess <juliettespiess@MacBook-Pro-de-Juliette.local>
2013-09-20 20:31:19 +0200

    Only process limited access article pages.

--- lemonde.js
+++ lemonde.js
@@ -1,9 +1,11 @@
-$.ajax({
-  type: "GET",
-  url: document.URL,
-}).done(function(response){
-  var original = $(response);
-  var article = document.getElementById('articleBody');
-  article.parentNode.replaceChild(original.find('#articleBody')[0], article);
-  document.getElementById('teaser_article').style.display = 'none';
-});
+if (document.getElementById('teaser_article')) {
+  $.ajax({
+    type: "GET",
+    url: document.URL,
+  }).done(function(response){
+    var original = $(response);
+    var article = document.getElementById('articleBody');
+    article.parentNode.replaceChild(original.find('#articleBody')[0], article);
+    document.getElementById('teaser_article').style.display = 'none';
+  });
+}
