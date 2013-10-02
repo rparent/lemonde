28258dfc58e6c6f9ce1ffd90ee29e670939ecb2b
Renobryce <renaud.parent@gmail.com>
2013-10-02 17:20:39 +0200

    Remove useless console log.

--- lemonde.js
+++ lemonde.js
@@ -7,7 +7,6 @@ if (teaserArticle) {
     var original = $(response).find('#articleBody')[0];
     var article = document.getElementById('articleBody');
     var zenButton = article.getElementsByClassName('btn_lecture_zen')[0];
-    console.log(zenButton)
     original.appendChild(zenButton)
     article.parentNode.replaceChild(original, article);
     teaserArticle.style.display = 'none';
