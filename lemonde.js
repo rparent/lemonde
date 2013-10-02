5675f9be3f5803e0aa762bab58a0b6f3854089c2
Renobryce <renaud.parent@gmail.com>
2013-10-02 15:09:36 +0200

    Remove form overlay in zen mode.

--- lemonde.js
+++ lemonde.js
@@ -1,11 +1,36 @@
-if (document.getElementById('teaser_article')) {
+var teaserArticle = document.getElementById('teaser_article');
+if (teaserArticle) {
   $.ajax({
-    type: "GET",
+    type: 'GET',
     url: document.URL,
   }).done(function(response){
     var original = $(response);
     var article = document.getElementById('articleBody');
     article.parentNode.replaceChild(original.find('#articleBody')[0], article);
-    document.getElementById('teaser_article').style.display = 'none';
+    teaserArticle.style.display = 'none';
   });
 }
+
+var zenButton = document.getElementsByClassName('btn_lecture_zen')[0];
+if (zenButton) {
+  zenButton.addEventListener('click', function() {
+    var overlayRemovalLoop = setInterval(function(){
+      removeOverlay()
+    }, 100);
+    setTimeout(function() {
+      stopLoopFunction(overlayRemovalLoop)
+    }, 5000);
+  }, false);
+}
+
+function removeOverlay() {
+  if (document.getElementsByClassName('overlay offre_decouverte')[0]) {
+    document.getElementsByClassName('overlay offre_decouverte')[0].style.display = 'none';
+    document.getElementsByClassName('fond_overlay offre_decouverte')[0].style.display = 'none';
+  }
+}
+
+function stopLoopFunction($myVar) {
+  clearInterval($myVar);
+}
+
