a0d766e7f0cf316a4316f3ae6423ead8ee45480b
Renobryce <renaud.parent@gmail.com>
2013-10-08 17:35:43 +0200

    Use css to hide overlay in zen mode.

--- lemonde.js
+++ lemonde.js
@@ -6,35 +6,7 @@ if (teaserArticle) {
   }).done(function(response){
     var original = $(response).find('#articleBody')[0];
     var article = document.getElementById('articleBody');
-    var zenButton = article.getElementsByClassName('btn_lecture_zen')[0];
-    if (zenButton) {
-      original.appendChild(zenButton)
-    }
     article.parentNode.replaceChild(original, article);
     teaserArticle.style.display = 'none';
   });
 }
-
-var zenButton = document.getElementsByClassName('btn_lecture_zen')[0];
-if (zenButton) {
-  zenButton.addEventListener('click', function() {
-    var overlayRemovalLoop = setInterval(function(){
-      removeOverlay()
-    }, 100);
-    setTimeout(function() {
-      stopLoopFunction(overlayRemovalLoop)
-    }, 10000);
-  }, false);
-}
-
-function removeOverlay() {
-  if (document.getElementsByClassName('overlay offre_decouverte')[0]) {
-    document.getElementsByClassName('overlay offre_decouverte')[0].style.display = 'none';
-    document.getElementsByClassName('fond_overlay offre_decouverte')[0].style.display = 'none';
-  }
-}
-
-function stopLoopFunction($myVar) {
-  clearInterval($myVar);
-}
-
