5bb1a84b1bb43c98f5daef552a14185d8891ee24
Renobryce <renaud.parent@gmail.com>
2013-10-08 17:57:53 +0200

    Add zenButton to restricted-access articles.

--- lemonde.js
+++ lemonde.js
@@ -6,6 +6,10 @@ if (teaserArticle) {
   }).done(function(response){
     var original = $(response).find('#articleBody')[0];
     var article = document.getElementById('articleBody');
+    var zenButton = article.getElementsByClassName('btn_lecture_zen')[0];
+    if (zenButton) {
+      original.appendChild(zenButton);
+    }
     article.parentNode.replaceChild(original, article);
     teaserArticle.style.display = 'none';
   });
